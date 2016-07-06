abc8642a733f2d5e5a147fb6ed39c45a2483768b
Mark Lyck <mark.lyck@gmail.com>
2016-07-05 22:56:52 -0500

    Added footer HTML and pagenation

--- dist/app.js
+++ dist/app.js
@@ -3,15 +3,26 @@ let $searchBtn = $('.search-button')
 let $searchBar = $('#search-bar')
 let $count = $('#results')
 let $categories = $('.categories').children('ul')
-let itemURL = 'https://api.etsy.com/v2/listings/active.js?api_key=bc7wp4hzrqhb0n0v3la55set&keywords=whiskey&includes=Images,Shop'
-// let categoriesURL = 'https://api.etsy.com/v2/taxonomy/categories.js?api_key=bc7wp4hzrqhb0n0v3la55set'
+let $pagenation = $('#pagenation')
+
+let $filterPrice = $('#filter-price')
+let $minPriceInput = $('#min-price')
+let $maxPriceInput = $('#max-price')
+let minPrice = ''
+let maxPrice = ''
+
+let apiKey = 'bc7wp4hzrqhb0n0v3la55set'
+let keywords = 'whiskey'
+let category = ''
+let baseURL = 'https://api.etsy.com/v2/listings/active.js?api_key=' + apiKey
+let apiURL = baseURL + category + '&keywords=' + keywords + '&includes=Images,Shop'
 
 let getItems = {
   type: 'GET',
   dataType: 'jsonp',
-  url: itemURL,
+  url: apiURL,
   success: function(response){
-    // console.log(response)
+    console.log(response);
     $itemGrid.empty()
     $count.text('(' + response.count + ' Results)') // TODO make this thousand seperated
     let item = response.results.map(result => {
@@ -24,6 +35,26 @@ let getItems = {
       $gridItem.hover(itemHover)
       $gridItem.children('.favorite-btn').on('click', (e) => $(e.target).toggleClass('favorited'))
     })
+    if (response.count/25 > 10) {
+      let pageCounter = response.count
+      let numberOfPages = 0
+      while(pageCounter > 0) {
+        pageCounter -= 25
+        numberOfPages++
+      }
+      let $lastPage = $('<li id="last-page"><a href="#"><i class="fa fa-chevron-left" aria-hidden="true"></a></li>')
+      $pagenation.append($lastPage)
+      for (var i = 1; i <= 8; i++) {
+        let $pageNumber = $('<li><a href="#">' + i + '</a></li>')
+        $pagenation.append($pageNumber)
+      }
+      let $pagesBetween = $('<li id="pages-between"><a href="#">...</a></li>')
+      $pagenation.append($pagesBetween)
+      let $finalPage = $('<li id="final-page"><a href="#">' + numberOfPages + '</a></li>')
+      $pagenation.append($finalPage)
+      let $nextPage = $('<li id="next-page"><a href="#"><i class="fa fa-chevron-right" aria-hidden="true"></a></li>')
+      $pagenation.append($nextPage)
+    }
   }
 }
 $.ajax(getItems)
@@ -34,34 +65,27 @@ function itemHover(e) {
 
 $searchBtn.on('click', () => {
   if ($searchBar.val() !== '') {
-    console.log($searchBar.val());
-    console.log('making AJAX request');
-    settings.url = 'https://api.etsy.com/v2/listings/active.js?api_key=bc7wp4hzrqhb0n0v3la55set&keywords=' + $searchBar.val() + '&includes=Images,Shop'
-    console.log(settings.url);
-    $.ajax(settings)
+    keywords = $searchBar.val()
+    getItems.url = getItems.url = baseURL + category + '&keywords=' + keywords + '&includes=Images,Shop'+ minPrice + maxPrice
+    $.ajax(getItems)
   }
 })
 
 $categories.children('li').on('click', function(){
-  let getItemsInCategory = {
-    type: 'GET',
-    dataType: 'jsonp',
-    url: 'https://api.etsy.com/v2/listings/active.js?api_key=bc7wp4hzrqhb0n0v3la55set&category=' + $(this).data().cat + '&keywords=whiskey&includes=Images,Shop',
-    success: function(response){
-      $itemGrid.empty()
-      $count.text('(' + response.count + ' Results)') // TODO make this thousand seperated
-      let item = response.results.map(result => {
-        // Return array with [title, image, url, price, shop-name]
-        return [result.title, result.Images[0].url_170x135, result.url, result.price, result.Shop.shop_name]
-      })
-      .forEach(item => {
-        let $gridItem = $('<li class="grid-item"><button class="favorite-btn" type="button"><i class="fa fa-heart" aria-hidden="true"></i></button><button class="list-btn" type="button"><i class="fa fa-bars" aria-hidden="true"></i><i class="fa fa-caret-down" aria-hidden="true"></i></button><a href="' + item[2] + '"><img src="' + item[1] + '"><div class="description"><p class="item-title">' + item[0] + '</p><div class="item-shop-and-price"><p>' + item[4] + '</p><p class="price">$' + item[3] + '</p></div></div></a></li>')
-        $itemGrid.append($gridItem)
-        $gridItem.hover(itemHover)
-        $gridItem.children('.favorite-btn').on('click', (e) => $(e.target).toggleClass('favorited'))
-      })
-    }
+  category = '&category=' + $(this).data().cat + '&'
+  getItems.url = getItems.url = baseURL + category + '&keywords=' + keywords + '&includes=Images,Shop'+ minPrice + maxPrice
+  $.ajax(getItems)
+})
+
+$filterPrice.on('click', function(){
+  if ($minPriceInput.val() !== '') {
+    minPrice = '&min_price=' + $minPriceInput.val()
   }
-  $.ajax(getItemsInCategory)
-  // console.log($(this).data().cat);
+  if ($maxPriceInput.val() !== '') {
+    maxPrice = '&max_price=' + $maxPriceInput.val()
+  }
+  if ($minPriceInput.val() !== '' || $maxPriceInput.val() !== '') {
+    getItems.url = baseURL + category + '&keywords=' + keywords + '&includes=Images,Shop' + minPrice + maxPrice
+    $.ajax(getItems)
+  }
 })
